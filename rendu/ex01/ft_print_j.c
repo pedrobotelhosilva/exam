185da66a5195aed89fc64ecbc52a12c14eed318a
Bárbara <barbararbbravo.21@gmail.com>
2025-09-18 15:42:12 -0300

    vai, nemo, vai

--- rendu/ex01/ft_print_j.c
+++ rendu/ex01/ft_print_j.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
-
-int	main(void)
+/*
+void	ft_print_j(void)
 {
 	write(1, "J", 1);
-	white(1, "/c", 1);
+	write(1, "\n", 1);
+}
+*/
+int	main(void)
+{
 	ft_print_j();
-	return(0)
+	return(0);
 }
